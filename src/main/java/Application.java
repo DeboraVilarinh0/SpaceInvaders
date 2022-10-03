392d9e9404f518ef3f7d0e53c84bc4d911e19a14
Carlos Teixeira <carloteixeira@deloitte.pt>
2022-10-03 14:50:37 +0100

    oitavo

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,17 @@
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        public static void main(String[] args) {
+            try {
+                Game game = new Game(40,20);
+                game.run();
 
+                //  Hero hero = new Hero(10,10);
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,20 +1,49 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.TerminalFactory;
+
+import java.io.IOException;
 
 public class Game {
 
 TerminalScreen screen;
 
-Game (){int Height, int Width){
+ Game () throws IOException {int Height,int Width) throws IOException {
 
+     TerminalSize terminalSize = new TerminalSize(Height,Width);
+     DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
 
-
-
+     Terminal terminal = TerminalFactory.createTerminal();
+     screen = new TerminalScreen(terminal);
+     screen.setCursorPosition(null); // we don't need a cursor
+     screen.startScreen();           // screens must be started
+     screen.doResizeIfNecessary();   // resize screen if necessary
+    // arena = new Arena(Height,Width);
+}
+     private void draw() throws IOException {
+         screen.clear();
+         arena.draw(screen.newTextGraphics());
+         screen.refresh();
+     }
+     public void run() throws IOException {
+         while (true) {
+             draw();
+             KeyStroke key = screen.readInput(); // read a keystroke
+             if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                 screen.close();
+             }
+             processKey(key);
+             if (key.getKeyType() == KeyType.EOF) {
+                 break;
+             }
+         }
+     }
 }
 
-
-
-
-}
-
-
-}
+    private void processKey(KeyStroke key) {
+    }
+    }
