cd5bbb38afcfa1351d5ede390fc3d116b4efd687
guisimoes <guisimoes@deloitte.pt>
2022-10-06 14:53:37 +0100

    antes da porcaria

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -58,7 +58,7 @@ public class Game {
                     break;
                 }}
 
-            if (startTime - lastMonsterMovement > 10) {
+            if (startTime - lastMonsterMovement > 500) {
                 arena.moveBadGuys();
                 arena.verifyBadGuysCollision();
                 draw();
